ad42d9dc766c9bd0e4518f8668bec4e378252712
fenabdel <abdeljalil.fenniri@gmail.com>
2022-08-24 16:38:03 +0100

    creating a backup files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// search
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+    document.getElementById("myOverlay").style.transitionDuration = "0.3s";
+}
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+
+}
