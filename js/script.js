301b284eca6c5da44ad9fc0c624e46522f68b77f
NAFIA Salma <salmanafae2@gmail.com>
2023-01-23 04:01:43 +0100

    Team and poles pages

--- js/script.js
+++ js/script.js
@@ -33,18 +33,25 @@ function closeSearch() {
 
 //OwlCarousel
 $(document).ready(function(){
-    $('.owl-carousel').find('.owl-item:first-child').addClass('first-item')
-    $('.owl-carousel').owlCarousel({
+        var owl = $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
+        margin:25,
         responsiveClass:true,
         center:true,
-        // items:5,
+        items: 5,
+        // auto play feature
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+
+        //
         nav:true,
         navText :[
             "<i class='fa fa-angle-left'></i>",
             "<i class='fa fa-angle-right'></i>",
         ],
+        onInitialized : startCarousel,
+
         responsive:{
             0:{
                 items:1,
@@ -61,7 +68,48 @@ $(document).ready(function(){
             }
         }
     })
-} )
+
+    owl.on('changed.owl.carousel', startCarousel);
+    
+
+    function startCarousel (event) {
+
+        setTimeout(function(){
+          var activeEls = $('.owl-item.active');
+          var activeItem = event.item.index;
+          var slideToCaption1 = activeEls.eq(0); // Change this value to target a different slide
+          var slideToCaption2 = activeEls.eq(4); // Change this value to target a different slide
+          setCarouselCaption1(slideToCaption1);
+          setCarouselCaption2(slideToCaption2);
+          setCarouselCaption3(activeItem);
+        },1);
+    
+        function setCarouselCaption1(el){
+            $(".owl-item").removeClass("target1");
+            el.addClass("target1");
+        }
+        function setCarouselCaption2(el){
+            $(".owl-item").removeClass("target2");
+            el.addClass("target2");
+        }
+        // function setCarouselCaption3(el){
+        //     if (el === 0) {
+        //         item1.style.display= 'block';
+        //         // item2.style.display= 'none';
+        //       } else {
+        //         item1.style.display= 'none';
+
+        //       }
+
+        //     // $('.AddTopaMargin').removeClass('active');
+        //     // $('#item-' + (activeItem + 1)).addClass('active');
+        // }
+    };
+    
+    
+    
+    
+    
 
 
 
@@ -71,63 +119,6 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Scroll right function
-function scrollRight() {
-    var container = document.querySelector('.TeamCartes');
-    container.scrollBy(350,0);
-}
-
-// Scroll left function
-function scrollLeft() {
-    var container = document.querySelector('.TeamCartes');
-    container.scrollBy(-550,0);
-}
-
-
-
-var items = document.querySelectorAll('.TeamCartes');
-var numberOfItemsToShow = 3;
-
-for (var i = numberOfItemsToShow; i < items.length; i++) {
-  items[i].style.display = 'none';
-}
-
-// // Get all the div elements
-// var items = document.querySelectorAll('.TeamCartes');
-
-// // Add an event listener to the window to detect scrolling
-// window.addEventListener('scroll', function() {
-//   // Get the current scroll position
-//   var scrollPosition = window.scrollY;
-
-//   // Loop through all the items
-//   for (var i = 0; i < items.length; i++) {
-//     // Get the position of the current item
-//     var itemPosition = items[i].getBoundingClientRect().top;
-
-//     // Check if the current item is in the middle of the page
-//     if (itemPosition > scrollPosition && itemPosition < scrollPosition + window.innerHeight) {
-//       // Apply the "active" class to the current item
-//       items[i].classList.add('active');
-//     } else {
-//       // Remove the "active" class from the current item
-//       items[i].classList.remove('active');
-//     }
-//   }
-// });
+});
 
 
